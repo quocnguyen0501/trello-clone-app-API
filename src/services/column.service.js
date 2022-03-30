a3218d524d0d553720c63127abc87755337ff27a
quocnguyen.forwork@gmail.com <0909343633Abc>
2022-03-30 14:59:34 +0700

    handle CRUD columns & cards from client and server

--- src/services/column.service.js
+++ src/services/column.service.js
@@ -1,4 +1,5 @@
 import { BoardsModel } from "../models/boards.model";
+import { cardModel } from "../models/card.model";
 import { columnsModel } from "../models/column.model"
 
 const createNew = async (data) => {
@@ -30,9 +31,26 @@ const update = async (id, data) => {
             ...data,
             updatedAt: Date.now()
         }
-        const result = columnsModel.update(id, updateData);
+        // Data send from client have an _id and card but store in DB don't need to save with structure -> check if _id and cards[] exist -> delete
+        if (updateData._id) {
+            delete updateData._id;
+        }
 
-        return result;
+        if (updateData.cards) {
+            delete updateData.cards;
+        }
+
+        const updatedColumn = await columnsModel.update(id, updateData);
+
+        if (updatedColumn._destroy) {
+            // Delete all card in this column
+            /**
+             * @param {array id of card for change state of method _destroy} updatedColumn.cardOrder
+             */
+            cardModel.deleteAllCardByColumn(updatedColumn.cardOrder)
+        }
+
+        return updatedColumn;
     } catch (error) {
         throw new Error(error);
     }
